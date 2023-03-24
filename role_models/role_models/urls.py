927534da5983945b5bd4b2940f4cde7c20b65da9
Angela11223344 <113986268+Angela11223344@users.noreply.github.com>
2023-03-24 08:23:08 +0800

    User detail view setup

--- role_models/role_models/urls.py
+++ role_models/role_models/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
     path('users/', include('users.urls')),
+    path('', include('profiles.urls')),
+
 ]
--- role_models/users/urls.py
+++ role_models/users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.CustomUserList.as_view(), name='users-list'),
+    #path('users/', views.CustomUserList.as_view(), name='users-list'),
+    path('', views.CustomUserList.as_view(), name='customuser-list'),
+	path('<int:pk>/', views.CustomUserDetail.as_view(), name='customuser-detail'),
 ]
