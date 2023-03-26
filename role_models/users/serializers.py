7e3f155a0e941ece930118fffe7179b80a3e53c4
KarthikaVC <karthikav.rmkcse@gmail.com>
2023-03-26 18:40:31 +0800

    Adding Code changes for Login issue and Token Generation - 26032023

--- role_models/users/serializers.py
+++ role_models/users/serializers.py
@@ -1,16 +1,34 @@
 from rest_framework import serializers
 from .models import Category, CustomUser
-
+from django.contrib.auth.password_validation import validate_password
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
     
-class CustomUserSerializer(serializers.ModelSerializer):
+class CustomUserSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    username = serializers.CharField(max_length=200)
+    email = serializers.EmailField()
+    first_name = serializers.CharField(max_length=200)
+    last_name = serializers.CharField(max_length=200)
+    password = serializers.CharField(write_only = True, required = True , validators =[validate_password])
     
     class Meta:
         model = CustomUser
-        fields = '__all__'
+        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
+        extra_kwargs = {'password': {'write_only': True}, "id": {"read_only": True}, 'first_name': {'required': True},'last_name': {'required': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+			email = validated_data['email'],
+			username = validated_data['username'],
+			first_name = validated_data['first_name'],
+			last_name = validated_data['last_name']
+		)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user    
 
 class CustomUserCategorySerializer(serializers.ModelSerializer):
     class Meta:
