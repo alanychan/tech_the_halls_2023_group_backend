1d07dd7a18835362e582f992f81670b01cf264b4
alanychan <amy_cct@yahoo.com>
2023-04-01 19:17:23 +0800

    Update to obtain auth token.

--- role_models/role_models/urls.py
+++ role_models/role_models/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 
 admin.site.index_title = 'Tech The Halls'
 admin.site.site_header = 'Tech The Halls'
@@ -12,5 +13,6 @@ urlpatterns = [
     path('', include('users.urls')),
     # path('', include('profiles.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('', include('hero.urls')),
 ]
