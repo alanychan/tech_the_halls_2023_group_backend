0b8387bff5156135c846c09c87343aa451e7b21f
alanychan <amy_cct@yahoo.com>
2023-04-02 19:26:51 +0800

    Add more permissions to control access to the views.

--- role_models/users/views.py
+++ role_models/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from rest_framework import status, permissions, generics, filters
+from rest_framework import status, generics, filters
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .permissions import  IsOwnerOrReadOnly, IsAdminOnly
