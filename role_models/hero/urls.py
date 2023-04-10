84f33915c1c70379543a123418b93e1165e167fb
alanychan <amy_cct@yahoo.com>
2023-04-10 12:10:05 +0800

    Remove ending slash in hero path.

--- role_models/hero/urls.py
+++ role_models/hero/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('hero/', views.HeroList.as_view(), name='hero-list'),
-    path('hero/<int:pk>/', views.hero_detail_view),
+    path('hero/<int:pk>', views.hero_detail_view),
 ]
